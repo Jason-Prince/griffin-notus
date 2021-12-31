27a96c953e1ef6325a6d614cf8a9d7a95cefe7dd
Jason Prince <jasoncprince@gmail.com>
2021-12-31 13:21:03 -0700

    created base pages

---
+++ pages/buildingcertification.jsx
@@ -0,0 +1,9 @@
+const BuildingCertification = () => {
+  return (
+    <>
+      <h1>BuildingCertification</h1>
+    </>
+  );
+};
+
+export default BuildingCertification;
---
+++ pages/zeronetenergy.jsx
@@ -0,0 +1,9 @@
+const ZeronetEnergy = () => {
+  return (
+    <>
+      <h1>ZeronetEnergy</h1>
+    </>
+  );
+};
+
+export default ZeronetEnergy;
